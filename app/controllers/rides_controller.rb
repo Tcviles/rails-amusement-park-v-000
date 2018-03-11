d85a42c277a6b8fe114bb2442831bf744ad710e3
Thomas Viles <thomas.c.viles.jr@gmail.com>
2018-03-11 23:28:16 +0000

    Done.

---
+++ app/controllers/rides_controller.rb
@@ -0,0 +1,13 @@
+class RidesController < ApplicationController
+  def new
+    @ride = Ride.create(ride_params)
+    @message = @ride.take_ride
+    redirect_to user_path(@ride.user, :message => @message)
+  end
+
+  private
+
+  def ride_params
+    params.permit(:user_id, :attraction_id)
+  end
+end
