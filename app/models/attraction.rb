5d34bfdb65b59a271ea1d98c74cd38b8665cd0c0
Thomas <Thomas.c.viles.jr@gmail.com>
2021-05-25 15:38:27 -0400

    Done.

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,4 +1,2 @@
-class Attraction < ActiveRecord::Base
-  has_many :rides
-  has_many :users, through: :rides
+class Attraction < ApplicationRecord
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  root to: 'static_pages#home'
-  resources :users
-  resources :attractions
-  get "/signin", to: "sessions#new"
-  post "/sessions/create", to: "sessions#create"
-  delete "/signout", to: "sessions#destroy"
-  post "/rides/new", to: "rides#new"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
